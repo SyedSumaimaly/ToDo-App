7bf3f4e2691f7d37c1ffbd0c63b349f62cf306be
Syed Sumaim Ali <96646046+SyedSumaimaly@users.noreply.github.com>
2022-09-02 21:41:08 +0500

    Add files via upload

---
+++ To Do App/app.js
@@ -0,0 +1,61 @@
+var list = document.getElementById("todo_list")
+var arr = [];
+var todoList = localStorage.getItem("todo-list");
+if (todoList) {
+    arr = JSON.parse(todoList);
+}
+
+function loadAllTodo() {
+    list.innerHTML = "";
+    for (var i = 0; i < arr.length; i++) {
+        var li = `
+        <li> ${arr[i]} 
+        <button onclick="deleteOne('${i}')">Del</button>
+        <button onclick="editTodo('${arr[i]}','${i}')">Edit</button> 
+        </li>
+        `;
+        list.innerHTML += li;
+    }
+}
+loadAllTodo();
+
+function addTodo() {
+    var input = document.getElementById("todo_value");
+    if (input.value.trim() !== "") {
+        var li = `
+        <li> ${input.value} 
+        <button onclick="deleteOne('${arr.length}')">Del</button>
+        <button onclick="editTodo('${input.value}','${arr.length}')">Edit</button> 
+        </li>
+        `;
+        arr.push(input.value);
+        localStorage.setItem("todo-list", JSON.stringify(arr));
+        list.innerHTML += li;
+        input.value = "";
+        input.focus();
+    };
+};
+
+
+function deleteOne(i) {
+    console.log(i);
+    arr.splice(i, 1);
+    localStorage.setItem("todo-list", JSON.stringify(arr));
+    event.target.parentNode.remove();
+    loadAllTodo();
+};
+
+
+function deleteAll() {
+    list.innerHTML = "";
+}
+
+function editTodo(a, i) {
+    // var oldValue = prompt("Enter updated value", a);
+    var oldValue = 
+    if (oldValue) {
+        arr.splice(i, 1, oldValue);
+        localStorage.setItem("todo-list", JSON.stringify(arr));
+        event.target.parentNode.firstChild.nodeValue = oldValue;
+    };
+};
